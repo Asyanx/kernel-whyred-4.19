77667d95135d1c90449991522e37f179eaf0637f
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2026-08-20 12:21:22 +0700

    sched/tune: Fix undefined schedtune_prefer_high_cap when SCHED_TUNE is disabled
    When CONFIG_SCHED_TUNE is disabled but CONFIG_UCLAMP_TASK_GROUP is enabled,
    schedtune_prefer_high_cap() is left undefined, causing an implicit
    declaration build error in fair.c.
    
    Move the fallback macro definition outside of the UCLAMP_TASK_GROUP
    ifdef block so it is always defined when CONFIG_SCHED_TUNE is disabled.
    Also clean up a duplicate macro definition for schedtune_task_boost().

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -37,8 +37,8 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu);
 #define schedtune_prefer_idle(tsk) uclamp_latency_sensitive(tsk)
 #else
 #define schedtune_prefer_idle(tsk) 0
-#define schedtune_prefer_high_cap(tsk) 0
 #endif
+#define schedtune_prefer_high_cap(tsk) 0
 
 #define schedtune_enqueue_task(task, cpu) do { } while (0)
 #define schedtune_dequeue_task(task, cpu) do { } while (0)
